1ad5910288d797461d099d8948578f0eea78a9f4
JustEndryu <59500925+JustEndryu@users.noreply.github.com>
2020-08-24 04:39:41 +0300

    Update UUIDMigration.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/migration/UUIDMigration.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/migration/UUIDMigration.java
@@ -52,7 +52,7 @@ public class UUIDMigration extends AbstractMigration {
     private static final Logger log = Logger.getLogger(UUIDMigration.class.getCanonicalName());
     private static final int LOG_DELAY = 5000;
 
-    private final Timer timer = new Timer("WorldGuard UUID Migration");
+    private final Timer timer = new Timer("Миграция UUID WorldGuard");
     private final ProfileService profileService;
     private final FlagRegistry flagRegistry;
     private final ConcurrentMap<String, UUID> resolvedNames = new ConcurrentHashMap<>();
@@ -76,14 +76,14 @@ public class UUIDMigration extends AbstractMigration {
 
     @Override
     protected void migrate(RegionDatabase store) throws MigrationException {
-        log.log(Level.INFO, "Миграция регионов в '" + store.getName() + "' для преобразования имен -> UUIDs...");
+        log.log(Level.INFO, "Миграция архива в '" + store.getName() + "' для преобразования имен -> UUIDs...");
 
         Set<ProtectedRegion> regions;
 
         try {
             regions = store.loadAll(flagRegistry);
         } catch (StorageException e) {
-            throw new MigrationException("Не удалось загрузить данные региона для мира '" + store.getName() + "'", e);
+            throw new MigrationException("Не удалось загрузить данные по регионам для всего мира '" + store.getName() + "'", e);
         }
 
         migrate(regions);
@@ -91,7 +91,7 @@ public class UUIDMigration extends AbstractMigration {
         try {
             store.saveAll(regions);
         } catch (StorageException e) {
-            throw new MigrationException("Не удалось сохранить данные региона после миграции мира '" + store.getName() + "'", e);
+            throw new MigrationException("Не удалось сохранить данные о регионах после миграции мира '" + store.getName() + "'", e);
         }
     }
 
